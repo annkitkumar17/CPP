e2080545ef79ad0d58006ddac73de961a5d86590
Ankit kumar <ankitcodes17@gmail.com>
2025-06-21 04:30:10 +0000

    Done

---
+++ Oparators.cpp
@@ -0,0 +1,16 @@
+// basic arithmatic operators
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{ int a=20 , b=10;
+    
+    cout << "sum: " << a+b << endl; 
+    cout << "diffrence : " << a-b << endl; 
+    cout << "product : " << a*b << endl; 
+    cout << "division : " << a/b << endl;
+    cout << "modulo : " << a%b << endl;
+
+    return 0;
+}
