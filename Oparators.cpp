be259013ffc9a84f6deb7d7c5d19b7e5a67a7c05
Ankit kumar <ankitcodes17@gmail.com>
2025-06-21 07:23:36 +0000

    done

--- Oparators.cpp
+++ Oparators.cpp
@@ -11,6 +11,22 @@ int main()
     cout << "product : " << a*b << endl; 
     cout << "division : " << a/b << endl;
     cout << "modulo : " << a%b << endl;
+    cout << (5/(double)2) << endl; // get answer in point do typecaste
+    
+    // Relationl oparator 
+
+   cout << (3<5) << endl; 
+   cout << (3>5) << endl;
+   cout << (3<=5) << endl;
+   cout << (3==3) << endl;
+   cout << (3!=5) << endl;
+
+   // Logical oparator 
+
+   cout << ( (3>1) || (3>5)) << endl; // OR
+   cout << ( (3>1) && (3>2)) << endl; // AND
+   
+    
 
     return 0;
 }
